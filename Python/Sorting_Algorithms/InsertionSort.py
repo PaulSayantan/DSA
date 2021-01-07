754e6579c16a31f87f5085302bbe06432a723eb5
Sayantan Paul <belikesayantan12@gmail.com>
2021-01-07 22:12:11 +0530

    Insertion Sort Implementation in Python

--- Python/Sorting_Algorithms/InsertionSort.py
+++ Python/Sorting_Algorithms/InsertionSort.py
@@ -5,7 +5,22 @@ from typing import List
 import unittest
 
 
-def insertionSort(arr: List[int]) -> List[int]:
+def insertion_sort(arr: List[int]) -> List[int]:
+    """
+    Insertion Sort Algorithm
+
+    T(n):
+        Best Case:  Ω(n)
+
+        Avg. Case:  Θ(n^2)
+
+        Worst Case: O(n^2)
+
+    S(n):   O(1)    Best for cases when we are short on memory space.
+
+    :param arr: An unsorted array of integers
+    :return:    sorted array of integers
+    """
     for j in range(1, len(arr)):
         key = arr[j]
         i = j - 1
@@ -19,19 +34,19 @@ def insertionSort(arr: List[int]) -> List[int]:
     return arr
 
 
-class sortingTest(unittest.TestCase):
+class SortingTest(unittest.TestCase):
 
     def test1(self):
-        self.assertEqual(insertionSort([4, 8, 3, 7, 9, 1]), [1, 3, 4, 7, 8, 9])
+        self.assertEqual(insertion_sort([4, 8, 3, 7, 9, 1]), [1, 3, 4, 7, 8, 9])
 
     def test2(self):
-        self.assertEqual(insertionSort([1, 1, 1, 0, 0]), [0, 0, 1, 1, 1])
+        self.assertEqual(insertion_sort([1, 1, 1, 0, 0]), [0, 0, 1, 1, 1])
 
     def test3(self):
-        self.assertEqual(insertionSort([2]), [2])
+        self.assertEqual(insertion_sort([2]), [2])
 
     def test4(self):
-        self.assertEqual(insertionSort([]), [])
+        self.assertEqual(insertion_sort([]), [])
 
 
 if __name__ == '__main__':
