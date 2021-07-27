a50e01dfe9ab576377629b4f704249d45d566832
Sayantan Paul <paulsayantan2022@gmail.com>
2021-07-27 20:58:39 +0530

    Max Heap in Python

--- Python/Data_Structures/Binary Heap/MaxHeap.py
+++ Python/Data_Structures/Binary Heap/MaxHeap.py
@@ -1,105 +1,104 @@
 # Max Heap Implementation in Python
 
+from typing import List
 class MaxHeap:
     def __init__(self):
-        self.heap = []
-        self.curr_size = 0
-
-    def swap(self, idx1, idx2):
-        """
-            Swapping two elements in the heap array
-        :param idx1:
-        :param idx2:
-        :return:
-        """
-        self.heap[idx1], self.heap[idx2] = self.heap[idx2], self.heap[idx1]
-
-    def heappush(self, data: int):
-        """
-           A push an item into the heap and perform heapify operation to maintain heap property
-        :param data:
-        :return:
-        """
-        self.heap.append(data)
-        self.curr_size += 1
-        self.heapify_up(self.curr_size - 1)
-
+        self.__heap = []
+    
+    # insert an element in the heap
+    def heappush(self, val: int) -> None:
+        self.__heap.append(val)
+        # maintain heap property
+        self.__heapify_up(len(self.__heap) - 1)
+    
+    # return the top, i.e. the maximum element in the heap
+    def getMax(self) -> int:
+        if self.__heap:
+            return self.__heap[0]
+    
+    # return and remove the top, i.e. the maximum element in the heap
     def heappop(self) -> int:
-        """
-            Pop out the max value present at top of the heap and maintain the heap property
-        :return: root element
-        """
-        self.swap(0, self.curr_size - 1)
-        popped_item = self.heap.pop(self.curr_size - 1)
-        self.curr_size -= 1
-        self.heapify_down(0)
-
-        return popped_item
-
-    def heapify(self):
-        """
-            Top-Down Heapify
-            Use, when all elements are added to the heap together
-        :return:
-        """
-        size = self.curr_size - 1
-        for idx in range(size // 2, -1, -1):
-            self.heapify_down(idx)
-
-    def heapify_up(self, child_idx):
-        parent_idx = (child_idx - 1) // 2
-        # if value at parent_idx is smaller than value at the child_idx, swap values
-        if self.heap[parent_idx] < self.heap[child_idx]:
-            self.swap(parent_idx, child_idx)
-
-        # when we have reached top of the heap: heapify is complete
-        if parent_idx <= 0:
-            return
+        # if heap size is greater than 1
+        if len(self.__heap) > 1:
+            # store the maximum element in a variable max
+            max = self.__heap[0]
+            # move the last element of the heap to the top
+            self.__heap[0] = self.__heap[-1]
+            # remove the last element
+            self.__heap.pop()
+            # maintain heap property
+            self.__heapify_down(0)
+            return max
+        elif len(self.__heap) == 1:
+            # store the maximum element in a variable max
+            max = self.__heap[0]
+            self.__heap.pop(0)
+            return max
         else:
-            # recursively continue heapify
-            self.heapify_up(parent_idx)
-
-    def heapify_down(self, parent_idx):
-        leftchild_idx = 2 * parent_idx + 1
-
-        # if no leftchild exists, we have reached the end of the heap
-        if leftchild_idx > len(self.heap) - 1:
+            return None
+    
+    # maintain heap property in down to up fashion
+    def __heapify_up(self, index) -> None:
+        # get parent index
+        parent = (index - 1) // 2
+        # stop iteration if index goes out of bound or check if parent is smaller than child
+        if index <= 0:
             return
+        elif self.__heap[parent] < self.__heap[index]:
+            # swap parent and child
+            tmp = self.__heap[parent]
+            self.__heap[parent] = self.__heap[index]
+            self.__heap[index] = tmp
+            # continue heapifying
+            self.__heapify_up(parent)
+    
+    # maintain heap property in up to down fashion
+    def __heapify_down(self, index) -> None:
+        # get index of left child and right child
+        left = (index * 2) + 1
+        right = (index * 2) + 2
+        # assume current item at index is largest
+        largest = index
+        # if leftchild is larger than index, update largest
+        if len(self.__heap) > left and self.__heap[largest] < self.__heap[left]:
+            largest = left
+        # if rightchild is larger than index, update largest
+        if len(self.__heap) > right and self.__heap[largest] < self.__heap[right]:
+            largest = right
+        # if largest is not index, swap and continue heapifying
+        if largest != index:
+            tmp = self.__heap[largest]
+            self.__heap[largest] = self.__heap[index]
+            self.__heap[index] = tmp
+            self.__heapify_down(largest)
+    
+    # build heap from an array of elements by heapifying each element
+    def buildHeap(self, arr: List[int]) -> None:
+        self.__heap = arr
+        for i in range(len(arr) - 1, -1, -1):
+            self.__heapify_down(i)
 
-        # if rightchild_idx exists and is greater than leftchild_idx, take the rightchild into consideration
-        if leftchild_idx + 1 <= len(self.heap) - 1 and self.heap[leftchild_idx] < self.heap[leftchild_idx + 1]:
-            leftchild_idx += 1
-
-        # if heap property violated, maintain by heapify
-        if self.heap[parent_idx] < self.heap[leftchild_idx]:
-            self.swap(parent_idx, leftchild_idx)
-            self.heapify_down(leftchild_idx)
-        else:
-            return
+    # return the maxheap as an array
+    def getHeap(self) -> List[int]:
+        return self.__heap
 
 
 if __name__ == '__main__':
     mxheap = MaxHeap()
 
-    # mxheap.heap = []
-    # mxheap.curr_size = 0
-    # mxheap.heappush(7)
-    # mxheap.heappush(10)
-    # mxheap.heappush(4)
-    # mxheap.heappush(3)
-    # mxheap.heappush(20)
-    # mxheap.heappush(15)
-    #
-    # print(mxheap.heap)
-
-    mxheap.heap = [7, 10, 4, 3, 20, 15]
-    mxheap.curr_size = 6
-    mxheap.heapify()
-    print(mxheap.heap)
+    mxheap.heappush(10)
+    mxheap.heappush(8)
+    mxheap.heappush(12)
+    mxheap.heappush(20)
+    mxheap.heappush(2)
+    mxheap.heappush(4)
+    
+    
+    print(mxheap.getHeap())
 
     print("Deleted Item: ", mxheap.heappop())
-    print(mxheap.heap)
+    print(mxheap.getHeap())
     print("Deleted Item: ", mxheap.heappop())
-    print(mxheap.heap)
+    print(mxheap.getHeap())
     print("Deleted Item: ", mxheap.heappop())
-    print(mxheap.heap)
+    print(mxheap.getHeap())
